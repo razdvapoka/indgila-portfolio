830806abaffef6970763b90de55fb09e288cec55
razdvapoka <razdvapoka@gmail.com>
2019-11-11 21:59:39 +0300

    :art: Blank targets and shorter description

--- src/components/markdown.js
+++ src/components/markdown.js
@@ -2,9 +2,15 @@ import preact from "preact";
 import Markup from "preact-markup";
 import marked from "marked";
 
+const renderer = new marked.Renderer();
+renderer.link = function(href, title, text) {
+  const link = marked.Renderer.prototype.link.apply(this, arguments);
+  return link.replace("<a", "<a target='_blank'");
+};
+
 const Markdown = ({ markdown, markupOpts = {}, markdownOpts = {}, ...rest }) => (
   <Markup
-    markup={marked(markdown, { breaks: true, ...markdownOpts })}
+    markup={marked(markdown, { breaks: true, renderer, ...markdownOpts })}
     trim={false}
     type="html"
     {...rest}
