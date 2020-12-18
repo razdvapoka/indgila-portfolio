8ec8a695f9827d6f844cc60a04d27dd3baaf9b8c
razdvapoka <razdvapoka@gmail.com>
2020-12-18 15:20:20 +0300

    :lipstick: Balance about columns

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -5,56 +5,35 @@ import Markdown from "../markdown";
 import content from "../../../content.json";
 import styles from "./styles.styl";
 
-const About = () => (
-  <Layout blogUrl={content.about.fields.blogUrl}>
-    <div className={styles.about}>
-      <Regular
-        className={styles.aboutDescription}
-        as={Markdown}
-        markdown={content.about.fields.description}
-      />
-      <Regular className={styles.aboutContacts}>
-        <span>{EMAIL}</span>
-        <br />
-        <span>{PHONE}</span>
-        <br />
-        <a href={FB} target="_blank" rel="noopener noreferrer">
-          facebook
-        </a>
-        <br />
-        <a href={IG} target="_blank" rel="noopener noreferrer">
-          instagram
-        </a>
-        <br />
-      </Regular>
-      <div className={styles.aboutLists}>
-        <div className={styles.aboutListsCol}>
-          <Small
-            className={styles.aboutList}
-            as={Markdown}
-            markdown={content.about.fields.experience}
-          />
-          <Small
-            className={styles.aboutList}
-            as={Markdown}
-            markdown={content.about.fields.lectures}
-          />
-        </div>
-        <div className={styles.aboutListsCol}>
-          <Small
-            className={styles.aboutList}
-            as={Markdown}
-            markdown={content.about.fields.exhibitions}
-          />
-          <Small
-            className={styles.aboutList}
-            as={Markdown}
-            markdown={content.about.fields.publications}
-          />
-        </div>
+const About = () => {
+  const { experience, lectures, exhibitions, publications } = content.about.fields;
+  const columnItemsMarkdown = [experience, lectures, exhibitions, publications].join("\n\n");
+  return (
+    <Layout blogUrl={content.about.fields.blogUrl}>
+      <div className={styles.about}>
+        <Regular
+          className={styles.aboutDescription}
+          as={Markdown}
+          markdown={content.about.fields.description}
+        />
+        <Regular className={styles.aboutContacts}>
+          <span>{EMAIL}</span>
+          <br />
+          <span>{PHONE}</span>
+          <br />
+          <a href={FB} target="_blank" rel="noopener noreferrer">
+            facebook
+          </a>
+          <br />
+          <a href={IG} target="_blank" rel="noopener noreferrer">
+            instagram
+          </a>
+          <br />
+        </Regular>
+        <Small as={Markdown} className={styles.aboutColumns} markdown={columnItemsMarkdown} />
       </div>
-    </div>
-  </Layout>
-);
+    </Layout>
+  );
+};
 
 export default About;
